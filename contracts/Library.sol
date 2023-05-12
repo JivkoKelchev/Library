f364ef70b89e927404d23df0a7933fa0f9624fd4
JivkoKelchev <jivko.r.kelchev@gmail.com>
2023-05-12 17:27:12 +0300

    Create a hardhat project for the Library.sol

--- contracts/Library.sol
+++ contracts/Library.sol
@@ -1,8 +1,7 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-pragma abicoder v2;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
 
-import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 import "hardhat/console.sol";
 
 contract Library is Ownable {
