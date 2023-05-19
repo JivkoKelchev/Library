9fafee178dc429e5ceb1104c8a34d2bcc01837d2
JivkoKelchev <jivko.r.kelchev@gmail.com>
2023-05-19 16:46:40 +0300

    Added borrow and return book interactions.

--- sdk/interact.ts
+++ sdk/interact.ts
@@ -13,41 +13,69 @@ async function run() {
     const contract = new ethers.Contract(contractAddress, compiledContractInterface.abi, wallet);
     if(!ethers.isAddress(contractAddress)) {
         console.log('Error: contractAddress is not valid.');
+        process.exitCode = 1;
     }
     if(!ethers.isAddress(wallet.address)) {
         console.log('Error wallet address is not valid');
+        process.exitCode = 1;
     }
-    
-    const balance = await provider.getBalance(wallet.address);
-    console.log(`Connected : ${wallet.address}`);
-    console.log(`Balance   : ${ethers.formatEther(balance)}`);
 
+    //print wallet info
     printSeparator();
-    
+    const balance = await provider.getBalance(wallet.address);
+    const roundedBalance = parseFloat(ethers.formatEther(balance)).toFixed(4);
+    console.log(`Connected : ${wallet.address}`);
+    console.log(`Balance   : ${roundedBalance} ETH`);
+
+    //print total books info
+    printSeparator();
     let booksCount: BigInt = await contract.booksCount();
-    
     //add books for interaction
     if (booksCount == BigInt(0)) {
-        const nonce = await provider.getTransactionCount(wallet.address);
-        await contract.addBook("test book 1", "test author", 1, { nonce });
-        await contract.addBook("test book 2", "test author", 2,  { nonce: nonce + 1 });
+        const addBookTx1 = await contract.addBook("test book 1", "test author", 1);
+        await addBookTx1.wait();
+        const addBookTx2 = await contract.addBook("test book 2", "test author", 2);
+        await addBookTx2.wait();
     }
+    
+    //print available books
     console.log(`Total books count in library is : ${booksCount.toString()}`);
     printSeparator();
     let availableBooks: string[] = await showAvailableBooks(contract);
-    // await contract.borrowBook(availableBooks[0]);
+    
+    // borrow book
     printSeparator();
-    // console.log('Books after borrowing the first book')
-    // showAvailableBooks(contract);
-
+    console.log(`Borrow book with id : ${availableBooks[0]}`)
+    let spinnerInterval: NodeJS.Timeout = startSpinner();
+    const borrowBookTx = await contract.borrowBook(availableBooks[0]);
+    await borrowBookTx.wait();
+    stopSpinner(spinnerInterval);
+    
+    //print available books after borrow the firs one
+    printSeparator();
+    await showAvailableBooks(contract);
+    
+    //print users current books
+    printSeparator();
     let currentBooks: string[] = await showUserCurrentBooks(contract, wallet.address);
+    
+    //return book
     printSeparator()
+    console.log(`Return book with id : ${currentBooks[0]}`);
+    spinnerInterval = startSpinner();
+    const returnBookTx = await contract.returnBook(currentBooks[0]);
+    await returnBookTx.wait();
+    stopSpinner(spinnerInterval);
 
+    //print available books after return
+    printSeparator();
+    await showAvailableBooks(contract);
+    printSeparator();
 
 }
 
 function printSeparator() {
-    console.log('------------------------------------')
+    console.log('------------------------------------------------------------------------')
 }
 
 async function showAvailableBooks(contract: Contract) {
@@ -71,9 +99,31 @@ async function showUserCurrentBooks(contract: Contract, userAddress: string) {
 async function printBook(contract: Contract, bookId: string) {
     let book: any[] = await contract.books(bookId);
     console.log(
-        `  BookId: ${bookId} \n  Title: ${book[0]} \n  Author: ${book[1]} \n  Available copies: ${book[2]} \n`
+        `  BookId    : ${bookId} \n  Title     : ${book[0]} \n  Author    : ${book[1]} \n  Available : ${book[2]} copies\n`
     );
 }
 
+// Function to start the spinner animation
+function startSpinner(): NodeJS.Timeout {
+    const spinnerChars = ['|', '/', '-', '\\'];
+    let i = 0;
+
+    const interval = setInterval(() => {
+        process.stdout.write(`\r${spinnerChars[i]} Processing transaction ...`);
+        i = (i + 1) % spinnerChars.length;
+    }, 100);
+
+    return interval;
+}
+
+// Function to stop the spinner animation
+function stopSpinner(interval: NodeJS.Timeout): void {
+    clearInterval(interval);
+    process.stdout.write('\rDone!                           \n'); // Clear the spinner line
+}
+
 console.log('Arguments:', process.argv.slice(2));
-run();
+run().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
